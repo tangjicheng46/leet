4ef614c437378098f6d0de63486fd9129b5bca1f
tangjicheng <tangjch15@gmail.com>
2023-10-30 23:38:50 +0900

    chore: update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
-#include <vector>
+#include <stack>
 
-// 76. Minimum Window Substring
+int longestValidParentheses(std::string s) {
+    std::stack<int> st;
+    int maxLength = 0;
+    int start = -1; // 记录当前有效括号子串的起始位置
 
-std::string minWindow(std::string s, std::string t) {
-  size_t m = s.size();
-  size_t n = t.size();
-  return ""
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            if (st.empty()) {
+                start = i; // 更新起始位置
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    maxLength = std::max(maxLength, i - start);
+                } else {
+                    maxLength = std::max(maxLength, i - st.top());
+                }
+            }
+        }
+    }
+
+    return maxLength;
 }
 
 int main() {
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+    std::string s = "(()))())(";
+    int result = longestValidParentheses(s);
+    std::cout << "Length of longest valid parentheses substring: " << result << std::endl;
+
+    return 0;
 }

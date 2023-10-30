48b7660c0cee706f0695694ed7583169833c3ac6
tangjicheng <tangjch15@gmail.com>
2023-10-31 00:01:53 +0900

    chore: update

--- main.cpp
+++ main.cpp
@@ -2,27 +2,25 @@
 #include <stack>
 
 int longestValidParentheses(std::string s) {
+    size_t maxLength = 0;
     std::stack<int> st;
-    int maxLength = 0;
-    int start = -1; // 记录当前有效括号子串的起始位置
-
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] == '(') {
-            st.push(i);
+    int start = -1;
+    for (size_t i = 0; i < s.size(); ++i) {
+      if (s[i] == '(') {
+        st.push(i);
+      } else {
+        if (st.empty()) {
+          start = i;
         } else {
-            if (st.empty()) {
-                start = i; // 更新起始位置
-            } else {
-                st.pop();
-                if (st.empty()) {
-                    maxLength = std::max(maxLength, i - start);
-                } else {
-                    maxLength = std::max(maxLength, i - st.top());
-                }
-            }
+          st.pop();
+          if (st.empty()) {
+            maxLength = std::max(maxLength, i - start);
+          } else {
+            maxLength = std::max(maxLength, i - st.top());
+          }
         }
+      }
     }
-
     return maxLength;
 }
 

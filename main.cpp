f616b8d442d224b3c369de7d848face40a2cef36
tangjicheng <tangjch15@gmail.com>
2023-10-30 19:32:54 +0900

    chore: update

--- main.cpp
+++ main.cpp
@@ -3,61 +3,15 @@
 #include <unordered_map>
 #include <vector>
 
-std::vector<int> findSubstring(std::string s, std::vector<std::string>& words) {
-  std::vector<int> result;
-  if (s.empty() || words.empty()) {
-    return result;
-  }
-  size_t sLen = s.size();
-  size_t wordCount = words.size();
-  size_t wordLen = words[0].size();
+// 76. Minimum Window Substring
 
-  std::unordered_map<std::string, int> wordsMap;
-  for (const std::string& iter : words) {
-    if (wordsMap.find(iter) != wordsMap.end()) {
-      wordsMap[iter] += 1;
-    } else {
-      wordsMap[iter] = 1;
-    }
-  }
-
-  for (size_t i = 0; i < wordLen; i++) {
-    int left = i;
-    int right = i;
-    std::unordered_map<std::string, int> currentWordsMap;
-    while (right + wordLen <= sLen) {
-      std::string word = s.substr(right, wordLen);
-      right += wordLen;
-      if (wordsMap.find(word) != wordsMap.end()) {
-        if (currentWordsMap.find(word) != currentWordsMap.end()) {
-          currentWordsMap[word] += 1;
-        } else {
-          currentWordsMap[word] = 1;
-        }
-        while (currentWordsMap[word] > wordsMap[word]) {
-          std::string leftWord = s.substr(left, wordLen);
-          left += wordLen;
-          currentWordsMap[leftWord] -= 1;
-        }
-        if (right - left == wordLen * wordCount) {
-          result.push_back(left);
-        }
-      } else {
-        currentWordsMap.clear();
-        left = right;
-      }
-    }
-  }
-  return result;
+std::string minWindow(std::string s, std::string t) {
+  size_t m = s.size();
+  size_t n = t.size();
+  return ""
 }
 
 int main() {
-  std::string s = "barfoothefoobarman";
-  std::vector<std::string> words = {"foo", "bar"};
-  std::vector<int> result = findSubstring(s, words);
-  for (int index : result) {
-    std::cout << index << " ";
-  }
-  std::cout << std::endl;
+  std::cout << "Hello world!" << std::endl;
   return 0;
 }

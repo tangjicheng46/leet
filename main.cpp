4234d9b722ce82cbc0f87d29d8cf9d4fd8d07bf6
tangjicheng <tangjch15@gmail.com>
2023-11-01 21:51:01 +0900

    chore: update

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
-  std::vector<std::vector<int>> result;
-  std::sort(nums.begin(), nums.end());
-  std::vector<int> start_vec;
-  backtrack(result, 0, start_vec, nums);
-  return result;
-}
-
 void backtrack(std::vector<std::vector<int>>& result, int start, std::vector<int>& current,
                const std::vector<int>& nums) {
   result.push_back(current);
@@ -23,8 +15,16 @@ void backtrack(std::vector<std::vector<int>>& result, int start, std::vector<int
   }
 }
 
+std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
+  std::vector<std::vector<int>> result;
+  std::sort(nums.begin(), nums.end());
+  std::vector<int> start_vec;
+  backtrack(result, 0, start_vec, nums);
+  return result;
+}
+
 int main() {
-  std::vector<int> nums = {1, 2, 2};
+  std::vector<int> nums = {1, 2, 2, 3};
   std::vector<std::vector<int>> result = subsetsWithDup(nums);
 
   // 打印结果
